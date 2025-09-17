20162d615c7d708d204323259efc3b7dc8440f02
Joel Lindström <test@fedora.com>
2025-09-17 18:56:41 +0200

    lade till den första metoden som tar 2 int argument

---
+++ Metoder/Program.cs
@@ -0,0 +1,20 @@
+﻿class Fuck_csharp()
+{
+  static void Main()
+  {
+    Console.WriteLine(Metoder.Arbiträr(2, 5));
+  }
+
+}
+
+
+
+
+class Metoder()
+{
+  public static int Arbiträr(int nummer1, int nummer2)
+  {
+    return nummer1 + nummer2;
+  }
+
+} 
